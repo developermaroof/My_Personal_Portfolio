64828ea7b713b3a386742808356c572ebe4f5632
Maroof Qureshi <developermaroof@gmail.com>
2024-10-15 13:21:48 +0500

    Updated Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,18 +15,30 @@ const Nav = () => {
         <div
           className="w-full bg-black/20 h-[96px] backdrop-blur-2xl
         rounded-full max-w-[460px] mx-auto px-5 flex justify-between
-        text-2xl text-white/50"
+        items-center text-2xl text-white/50"
         >
-          <Link>
+          <Link
+            className="cursor-pointer w-[60px] h-[60px] flex
+          items-center justify-center"
+          >
             <BiHomeAlt />
           </Link>
-          <Link>
+          <Link
+            className="cursor-pointer w-[60px] h-[60px] flex
+          items-center justify-center"
+          >
             <BiUser />
           </Link>
-          <Link>
+          <Link
+            className="cursor-pointer w-[60px] h-[60px] flex
+          items-center justify-center"
+          >
             <BsClipboardData />
           </Link>
-          <Link>
+          <Link
+            className="cursor-pointer w-[60px] h-[60px] flex
+          items-center justify-center"
+          >
             <BsBriefcase />
           </Link>
         </div>
