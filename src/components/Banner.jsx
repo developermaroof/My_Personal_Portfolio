5024b2116fc154078ed6ac344d408ae52c817a62
Maroof Qureshi <developermaroof@gmail.com>
2024-10-16 12:41:11 +0500

    Updated the Header and Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -13,16 +13,27 @@ import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section className="section" id="home">
+    <section className="min-h-[85vh] lg:min-h-[78vh] flex items-center" id="home">
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
           {/* text */}
-          <div>
-            <h1>
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[1] lg:text-[110px]">
               Maroof <span>Qureshi</span>
-            </h1>
-            <div className="">
-            <span className="mr-1">I am a </span>
+            </motion.h1>
+            <motion.div 
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary 
+              font-semibold uppercase leading-[1]">
+            <span className="text-white mr-1">I am a </span>
             <TypeAnimation sequence={[
               "Frontend Developer.",
               2000,
@@ -36,12 +47,55 @@ const Banner = () => {
             wrapper="span"
             repeat={Infinity}
             />
-            </div>
+            </motion.div>
+            <motion.p 
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0">
+              A seasoned web developer with 70+ completed projects, specializing in
+              UI/UX design. Deliver high-quality, innovative solutions, setting a
+              standard for excellence in every project.
+            </motion.p>
+            <motion.div 
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">My Portfolio</a>
+            </motion.div>
+            {/* socials */}
+            <motion.div 
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaLinkedin />
+              </a>
+              <a href="#">
+                <FaFacebook />
+              </a>
+              <a href="#">
+                <FaInstagram />
+              </a>
+            </motion.div>
           </div>
           {/* image */}
-          <div>
+          <motion.div 
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={'show'}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
             <img src={image} alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,7 @@ const Header = () => {
         <div className="flex justify-between items-center">
           <a href="#">
           <span
-            className="font-bold bg-gradient-to-r from-[#ff56f6]
-            via-[#b936ee] to-[#3bace2] bg-clip-text text-transparent"
+            className="text-gradient"
           >
             {"< Maroof />"}
           </span>
